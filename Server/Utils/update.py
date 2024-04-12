4e45741b1c212d14fc14b711255bad475b1765fd
hanyuduan0131 <d767383823@gmail.com>
2024-04-12 17:30:38 -0400

    1

---
+++ Server/Utils/update.py
@@ -0,0 +1,51 @@
+class UpdateContact:
+
+    def __init__(self, rq, name, ip_address, udp_socket, **_):
+        self.TYPE = "UPDATE-CONTACT"
+        self.rq = rq
+        self.name = name
+        self.ip_address = ip_address
+        self.udp_socket = udp_socket
+
+    def __str__(self):
+        return f"""
+        {self.TYPE}
+        RQ#:\t{self.rq}
+        Name:\t{self.name}
+        IP Address:\t{self.ip_address}
+        UDP socket#:\t{self.udp_socket} 
+        """
+
+class UpdateConfirmed:
+
+    def __init__(self, rq, name, ip_address, udp_socket, **_):
+        self.TYPE = "UPDATE-CONFIRMED"
+        self.rq = rq
+        self.name = name
+        self.ip_address = ip_address
+        self.udp_socket = udp_socket
+
+    def __str__(self):
+        return f"""
+        {self.TYPE}
+        RQ#:\t{self.rq}
+        Name:\t{self.name}
+        IP Address:\t{self.ip_address}
+        UDP socket#:\t{self.udp_socket} 
+        """
+
+class UpdateDenied:
+
+    def __init__(self, rq, name, reason, **_):
+        self.TYPE = "UPDATE-DENIED"
+        self.rq = rq
+        self.name = name
+        self.reason = reason
+
+    def __str__(self):
+        return f"""
+        {self.TYPE}
+        RQ#:\t{self.rq}
+        Name:\t{self.name}
+        Reason:\t{self.reason}
+        """
